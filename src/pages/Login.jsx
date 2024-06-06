402dfe5ec0ef9750cabf1188c93b32bebea52530
Rakshith Acharya <rakshixh@gmail.com>
2024-06-06 11:12:58 +0530

    updated login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,59 +1,86 @@
-import React, {useState, useEffect} from 'react'
-import MiniHeader from '../components/MiniHeader'
-import Header from '../components/Header'
-import Signin from '../components/SignIn'
-import Profile from '../components/Profile'
-import Footer from '../components/Footer'
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import MiniHeader from "../components/MiniHeader";
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+import LoginCSS from "../css/login.module.css";
 
 const Login = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userType, setUserType] = useState('');
-    
-    // Check for previous login state in localStorage on component mount 
-    useEffect(() => {
-    const loggedIn = localStorage.getItem('isLoggedIn');
-    const userType = localStorage.getItem('userType');
-    
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userType, setUserType] = useState("");
+
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("isLoggedIn");
+    const userType = localStorage.getItem("userType");
+
     if (loggedIn && userType) {
       setIsLoggedIn(JSON.parse(loggedIn));
       setUserType(JSON.parse(userType));
     }
-    }, []);
+  }, []);
 
-    const handleLogin = (type) => {
-        setUserType(type);
-        setIsLoggedIn(true);
-        localStorage.setItem('isLoggedIn', JSON.stringify(true));
-        localStorage.setItem('userType', JSON.stringify(type));
+  const handleLogin = (type) => {
+    setUserType(type);
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", JSON.stringify(true));
+    localStorage.setItem("userType", JSON.stringify(type));
 
-        if(type === 'user'){
-          const address = 'Akshya Nagar, 1st Block, 1st Cross, Ramamurthy nagar, Bangalore-560016'
-          localStorage.setItem('Address', address);
-        } 
+    if (type === "user") {
+      const address =
+        "Akshya Nagar, 1st Block, 1st Cross, Ramamurthy Nagar, Bangalore-560016";
+      localStorage.setItem("Address", address);
     }
 
-    const handleLogout = () => {
-        setIsLoggedIn(false);
-        setUserType('');
-        localStorage.removeItem('isLoggedIn');
-        localStorage.removeItem('userType');
-        localStorage.removeItem('Address');
-        localStorage.removeItem('wishlist');
-    };
+    if (type === "guest") {
+      toast.success("You logged in as Guest!");
+    } else if (type === "user") {
+      toast.success("You logged in as a User!");
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserType("");
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("userType");
+    localStorage.removeItem("Address");
+    localStorage.removeItem("wishlist");
+  };
+
   return (
-    <>
-    <MiniHeader/>
-    <Header/>
+    <div className={LoginCSS.page}>
+      <MiniHeader />
+      <Header />
+      <div className={LoginCSS.content}>
+        {isLoggedIn ? (
+          <Profile userType={userType} handleLogout={handleLogout} />
+        ) : (
+          <div className={LoginCSS.container}>
+            <div className={LoginCSS.card}>
+              <h1 className={LoginCSS.loginTitle}>Login</h1>
+              <button
+                className={LoginCSS.btn}
+                onClick={() => handleLogin("guest")}
+              >
+                Login as Guest
+              </button>
+              <button
+                className={LoginCSS.btn}
+                onClick={() => handleLogin("user")}
+              >
+                Login as User
+              </button>
+              <p className={LoginCSS.terms}>
+                You are agreed to our Terms and Conditions!
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-    {isLoggedIn ? (
-        <Profile userType={userType} handleLogout={handleLogout} />
-      ) : (
-        <Signin handleLogin={handleLogin} />
-    )}
-
-    <Footer/>
-    </>
-  )
-}
-
-export default Login
+export default Login;
