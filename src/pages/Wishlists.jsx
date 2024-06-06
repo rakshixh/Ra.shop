32c09ffe0667f30d94f0ae5d38b30ae964747915
Rakshith Acharya <rakshixh@gmail.com>
2024-06-06 11:27:17 +0530

    updated product go to product btn style in wishlist page

--- src/pages/Wishlists.jsx
+++ src/pages/Wishlists.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import MiniHeader from '../components/MiniHeader'
-import Header from '../components/Header'
-import ProductData from '../ProductData'
-import Footer from '../components/Footer'
-import HomeCSS from '../css/home.module.css'
-import WishlistEmpty from '../assets/emptycart.svg'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MiniHeader from "../components/MiniHeader";
+import Header from "../components/Header";
+import ProductData from "../ProductData";
+import Footer from "../components/Footer";
+import HomeCSS from "../css/home.module.css";
+import WishlistEmpty from "../assets/emptycart.svg";
 
 function Wishlists() {
   const [wishlist, setWishlist] = useState([]);
@@ -15,57 +15,78 @@ function Wishlists() {
       // To Remove the product from wishlist
       const updatedWishlist = wishlist.filter((id) => id !== productId);
       setWishlist(updatedWishlist);
-      localStorage.setItem('wishlist', JSON.stringify(updatedWishlist));
+      localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
     } else {
       // Add product to wishlist
       const updatedWishlist = [...wishlist, productId];
       setWishlist(updatedWishlist);
-      localStorage.setItem('wishlist', JSON.stringify(updatedWishlist));
+      localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
     }
-  }
+  };
 
   useEffect(() => {
-    const storedWishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+    const storedWishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
     setWishlist(storedWishlist);
   }, []);
 
   // Filtering the ProductData array to display only the wishlist products
-  const wishlistProducts = ProductData.filter((product) => wishlist.includes(product.id));
+  const wishlistProducts = ProductData.filter((product) =>
+    wishlist.includes(product.id)
+  );
 
   return (
     <>
-    <MiniHeader/>
-    <Header/>
-    {wishlistProducts.length === 0 ? (
-      <div className={HomeCSS.EmptyWishlistContainer}>
-        <div className={HomeCSS.imagecontainer}>
-          <img src={WishlistEmpty} className={HomeCSS.emptywishlistimage} alt='Empty Cart'/>
-        </div>
-        <h1 align='center' style={{color:'#2D6A4F'}}>Create your wishlist and save your favorite products!</h1>
-      </div>
-    ) : (
-      <div className={HomeCSS.wishlistContainer}>
-        <div className={HomeCSS.productContainer}>
-          {wishlistProducts.map((product) => (
-          <div key={product.id} className={HomeCSS.productCard}>
-            <img src={product.img} className={HomeCSS.productImg} alt={product.title}/>
-            <div className={HomeCSS.productTitlePriceDiv}>
-                <h2 className={HomeCSS.title}>{product.title}</h2>
-                <p className={HomeCSS.price}>Price: ₹<b>{product.price}</b></p>
-                <button className={HomeCSS.wishlistBtn} onClick={() => handleWishlistToggle(product.id)}>
-                  {wishlist.includes(product.id) ? 'Remove from Wishlist' : 'Add to Wishlist'}
-                </button>
-                <Link to={`/products/${product.id}`} className={HomeCSS.GTPbtn}><button className={HomeCSS.GTPbutton}>Go To Product</button></Link>
-            </div>
+      <MiniHeader />
+      <Header />
+      {wishlistProducts.length === 0 ? (
+        <div className={HomeCSS.EmptyWishlistContainer}>
+          <div className={HomeCSS.imagecontainer}>
+            <img
+              src={WishlistEmpty}
+              className={HomeCSS.emptywishlistimage}
+              alt="Empty Cart"
+            />
           </div>
-          ))}
+          <h1 align="center" style={{ color: "#2D6A4F" }}>
+            Create your wishlist and save your favorite products!
+          </h1>
         </div>
-      </div>
-    )}
+      ) : (
+        <div className={HomeCSS.wishlistContainer}>
+          <div className={HomeCSS.productContainer}>
+            {wishlistProducts.map((product) => (
+              <div key={product.id} className={HomeCSS.productCard}>
+                <img
+                  src={product.img}
+                  className={HomeCSS.productImg}
+                  alt={product.title}
+                />
+                <div className={HomeCSS.productTitlePriceDiv}>
+                  <h2 className={HomeCSS.title}>{product.title}</h2>
+                  <p className={HomeCSS.price}>
+                    Price: ₹<b>{product.price}</b>
+                  </p>
+                  <button
+                    className={HomeCSS.wishlistBtn}
+                    onClick={() => handleWishlistToggle(product.id)}
+                  >
+                    {wishlist.includes(product.id)
+                      ? "Remove from Wishlist"
+                      : "Add to Wishlist"}
+                  </button>
+                </div>
+                <Link to={`/products/${product.id}`} className={HomeCSS.GTPbtn}>
+                  <button className={HomeCSS.GTPbutton}>Go To Product</button>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
 
-    <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Wishlists
+export default Wishlists;
